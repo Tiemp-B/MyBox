a8485586009dc7b878b0011dd69dedabf9999b8d
Tiemp-B <bjh819819@naver.com>
2025-01-11 22:31:35 +0900

    10384 update

--- acmicpc/10384.cpp
+++ acmicpc/10384.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void func(){
+void func(int T){
 
 	string str;
 	getline(str, cin);
@@ -10,7 +10,7 @@ void func(){
 	int ans =0;
 
 
-	
+	cout << "Case "<<T <<": ";
 	switch(ans){
 		case 0:
 			cout << "Not a pangram"<<endl;
